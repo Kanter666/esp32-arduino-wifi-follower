ea1dbc2cc55f2f91db878194cc23ef0028710e16
Kanter666 <ak2g15@soton.ac.uk>
2017-10-06 20:12:41 +0100

    basic turning

--- show_wifi.ino
+++ show_wifi.ino
@@ -2,6 +2,8 @@
 
 //name of the network
 const char* ssid = "Why is Bill an idiot";
+float oldDistance = - 100;
+int state = 1; //1 = forward, 2 = backward, 3 = left, 4 = right
  
 void setup() {
   //setup
@@ -12,16 +14,57 @@ void setup() {
   delay(100);
 
   Serial.println("Setup done");
- 
+
 }
- 
-void loop() {
-  //scanning netwroks and printing strength of 'ssid' netwrok
-  int n = WiFi.scanNetworks();
-  for (int i = 0; i < n; ++i) {
-    if(WiFi.SSID(i) == ssid){
-      Serial.println(WiFi.RSSI(i));
+
+// calculate strength of signal by getting average of 5 measurements
+float signalStrength(){
+  float total = 0;
+  for (int j = 0; j < 5; j++){
+    int n = WiFi.scanNetworks();
+    for (int i = 0; i < n; ++i) {
+      if(WiFi.SSID(i) == ssid){
+        total += WiFi.RSSI(i);
+        break;
+      }
     }
   }
-  delay(1);
+  total = total/5;
+  return total;
 }
+
+void goDir(int dir) {
+  switch (dir)
+  {
+    case 1: 
+      Serial.println("forward");
+      break;
+    case 2:
+      Serial.println("back");
+      break;
+    case 3:
+      Serial.println("left");
+      break;
+    case 4:
+      Serial.println("right");
+      break;
+  }
+}
+
+void loop() {
+  
+  //scanning netwroks and printing strength of 'ssid' netwrok
+  float strength = signalStrength();
+
+  if(strength>oldDistance){
+    state = 1;
+  }else{
+    if(state==1){
+      state=3;
+    }
+  }
+  goDir(state);
+  Serial.println(strength);
+  oldDistance = strength;
+}
+
